a3ca86149a7e60dcd9d4ae7e2d7fb0b62b97a705
lucas-sdalmeida <lucas.lucas.sa@hotmail.com>
2023-10-27 13:59:25 -0300

    feat: added activity binding to MainActivity

--- app/src/main/java/com/lucassdalmeida/splitthebill/MainActivity.kt
+++ app/src/main/java/com/lucassdalmeida/splitthebill/MainActivity.kt
@@ -2,10 +2,13 @@ package com.lucassdalmeida.splitthebill
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.lucassdalmeida.splitthebill.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val activityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(activityMainBinding.root)
     }
 }
