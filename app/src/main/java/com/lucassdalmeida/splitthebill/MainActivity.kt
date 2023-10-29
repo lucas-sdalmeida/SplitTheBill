c191c9cbe757b4c4b79dbcec45f9357a99ea5af0
lucas-sdalmeida <lucas.lucas.sa@hotmail.com>
2023-10-29 16:00:36 -0300

    feat: created a options menu and inflated it

--- app/src/main/java/com/lucassdalmeida/splitthebill/MainActivity.kt
+++ app/src/main/java/com/lucassdalmeida/splitthebill/MainActivity.kt
@@ -2,6 +2,7 @@ package com.lucassdalmeida.splitthebill
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import com.lucassdalmeida.splitthebill.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +11,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(activityMainBinding.root)
+        setSupportActionBar(activityMainBinding.mainToolbar.appToolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
     }
 }
